ef51f5a0e94d4bfb2badfb6a6882ada378461322
rezha4 <rezha.bahari@gmail.com>
2025-01-19 20:32:10 +0700

    chore: crud test

--- cypress/e2e/crud.cy.ts
+++ cypress/e2e/crud.cy.ts
@@ -1,6 +1,41 @@
-describe('CRUD tests', () => {
-  it('Creates', () => {
-    cy.visit('http://localhost:3000/crud');
+describe("CRUD tests", () => {
+  beforeEach(() => cy.visit("http://localhost:3000/crud"));
 
-  })
-})
+  it("Create", () => {
+    cy.get("input").type("lucius malfoy");
+    cy.contains("button", "Create").click();
+    cy.get("li").should("contain.text", "lucius malfoy");
+  });
+
+  it("Read", () => {
+    cy.contains("li", "lucius malfoy").find("button").contains("view").click();
+
+    cy.on("window:alert", (alertText) => {
+      const alertData = JSON.parse(alertText);
+      expect(alertData).to.have.property("name", "lucius malfoy");
+    });
+  });
+
+  it("Update", () => {
+    // Stub the prompt before clicking the update button
+    cy.window().then((win) => {
+      cy.stub(win, "prompt").returns("severus snape");
+    });
+
+    cy.contains("li", "lucius malfoy")
+      .find("button")
+      .contains("update")
+      .click();
+
+    cy.get("li").should("contain.text", "severus snape");
+  });
+
+  it("Delete", () => {
+    cy.contains("li", "severus snape")
+      .find("button")
+      .contains("delete")
+      .click();
+
+    cy.get("li").should("not.contain.text", "severus snape");
+  });
+});
